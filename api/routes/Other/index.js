91b16d04e5eb53f7e1e4e2bc0ab71c2ba0d82335
Rose Parry <rose729parry@gmail.com>
2025-04-13 06:30:47 +0200

    Change routes

--- api/routes/Other/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const VerifyHashURL = require("./services/VerifyHashURL");
-const GetChatbotURL = require("./services/GetChatbotURL");
-const router = express.Router();
-
-router.post("/verifyHashURL", VerifyHashURL);
-router.post("/getChatbotURL", GetChatbotURL);
-
-module.exports = router;
